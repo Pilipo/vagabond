53427675d09971e45239d9b76c7405c0c75e451d
Pilipo <pl@philliplehner.com>
2020-07-07 09:17:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AWSTest from './components/aws'
+
+function App() {
+  return (
+    <div className="App">
+      <AWSTest />
+    </div>
+  );
+}
+
+export default App;
