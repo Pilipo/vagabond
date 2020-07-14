f2c747be6b8b467611f7f5e196f385b8e71df55a
Pilipo <pl@philliplehner.com>
2020-07-14 15:18:01 -0500

    Styles the signup page

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -5,9 +5,30 @@ import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpPage = () => (
-  <div>
-    <h1>Sign Up</h1>
-    <SignUpForm />
+  <div className="container">
+    <div className="card o-hidden border-0 shadow-lg my-5">
+      <div className="card-body p-0">
+        <div className="row">
+          <div className="col-lg-5 d-none d-lg-block bg-register-image"></div>
+          <div className="col-lg-7">
+            <div className="p-5">
+              <div className="text-center">
+                <h1 className="h4 text-gray-900 mb-4">Create an Account!</h1>
+              </div>
+              <SignUpForm />
+              <hr/ >
+              <div className="text-center">
+                    <Link className="small" to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+                  </div>
+                  <div className="text-center">
+                    <Link className="small" to={ROUTES.SIGN_IN}>Already have an account? Login!</Link>
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 );
 
@@ -58,36 +79,54 @@ class SignUpFormBase extends Component {
     const isInvalid = passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === '';
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">Sign Up</button>
+      <form className="user" onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <input
+            id="exampleFirstName"
+            className="form-control form-control-user"
+            name="username"
+            value={username}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Full Name"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control form-control-user"
+            id="exampleInputEmail"
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+          />
+        </div>
+        <div className="form-group row">
+          <div className="col-sm-6 mb-3 mb-sm-0">
+            <input
+              className="form-control form-control-user"
+              id="exampleInputPassword"
+              name="passwordOne"
+              value={passwordOne}
+              onChange={this.onChange}
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="col-sm-6">
+            <input
+              className="form-control form-control-user"
+              id="exampleRepeatPassword"
+              name="passwordTwo"
+              value={passwordTwo}
+              onChange={this.onChange}
+              type="password"
+              placeholder="Repeat Password"
+            />
+          </div>
+        </div>
+        <button disabled={isInvalid} className="btn btn-primary btn-user btn-block" type="submit">Register Account</button>
 
         {error && <p>{error.message}</p>}
       </form>
